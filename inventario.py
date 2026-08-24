ac21f8675b87788d05bcb4744399a85205a009c1
Yimmer Alexis Obando Ballen <obandoyimer4@gmail.com>
2026-08-24 07:50:15 -0500

    Primer sistema de inventario

---
+++ inventario.py
@@ -0,0 +1,12 @@
+print("================================")
+print("     SISTEMA DE INVENTARIO")
+print("================================")
+
+producto = input("Ingrese el nombre del producto: ")
+cantidad = int(input("Ingrese la cantidad: "))
+precio = float(input("Ingrese el precio: "))
+
+print("\n--- INFORMACIÓN DEL PRODUCTO ---")
+print("Producto:", producto)
+print("Cantidad:", cantidad)
+print("Precio: $", precio)
